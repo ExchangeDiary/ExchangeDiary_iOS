03b289905272f6d9a49b50d43e3b0dfba50e157f
전소영 <jsu3417@gmail.com>
2021-07-22 19:11:49 +0900

    [#7] 녹음 시작시 정지 버튼으로 변경

--- VODA_iOS/VODA_iOS/Sources/Audio Record/ViewController/RecordSoundViewController.swift
+++ VODA_iOS/VODA_iOS/Sources/Audio Record/ViewController/RecordSoundViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundViewController: UIViewController {
-
+    @IBOutlet weak var recordSeconds: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordGuideText: UILabel!
+    
+    var audioRecorder: AVAudioRecorder?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
        
     }
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        recordButton.setImage(UIImage(named: "stop"), for: .normal)
+        recordGuideText.isHidden = true
+        
+    }
+}
+
+// MARK: AVAudioRecorderDelegate
+extension RecordSoundViewController: AVAudioRecorderDelegate {
+    
 }
