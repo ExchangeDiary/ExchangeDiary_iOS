fcde59d89c9edde9b360856144a10c7095f48e21
전소영 <jsu3417@gmail.com>
2021-07-22 18:15:50 +0900

    [#7] audio test UI 추가

---
+++ VODA_iOS/VODA_iOS/Sources/Audio Record/ViewController/PlaySoundViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySoundViewController.swift
+//  VODA_iOS
+//
+//  Created by 전소영 on 2021/07/22.
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
---
+++ VODA_iOS/VODA_iOS/Sources/Audio Record/ViewController/RecordSoundViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordSoundViewController.swift
+//  VODA_iOS
+//
+//  Created by 전소영 on 2021/07/22.
+//
+
+import UIKit
+
+class RecordSoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
