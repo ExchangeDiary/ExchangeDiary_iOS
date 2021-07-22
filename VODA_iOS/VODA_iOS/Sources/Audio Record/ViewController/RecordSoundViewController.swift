d7e86c52d5e6738a1498817c7fe20970b8c5e3ee
전소영 <jsu3417@gmail.com>
2021-07-22 22:41:35 +0900

    [#7] 녹음 기능 추가

--- VODA_iOS/VODA_iOS/Sources/Audio Record/ViewController/RecordSoundViewController.swift
+++ VODA_iOS/VODA_iOS/Sources/Audio Record/ViewController/RecordSoundViewController.swift
@@ -15,21 +15,49 @@ class RecordSoundViewController: UIViewController {
     
     var audioRecorder: AVAudioRecorder?
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
     }
     
     @IBAction func recordAudio(_ sender: Any) {
         recordButton.setImage(UIImage(named: "stop"), for: .normal)
         recordGuideText.isHidden = true
         
+        let directoryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        print(directoryPath)
+        let recordingName = "recordedVoice.m4a"
+        
+        let pathArray = [directoryPath, recordingName]
+        guard let filePath = URL(string: pathArray.joined(separator: "/")) else {
+            return
+        }
+        print(filePath)
+        
+        let recordSettings = [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+                              AVEncoderBitRateKey: 32,
+                              AVNumberOfChannelsKey: 1,
+                              AVSampleRateKey: 12000] as [String : Any]
+        
+        let session = AVAudioSession.sharedInstance()
+        try? session.setCategory(AVAudioSession.Category.playAndRecord, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+        
+        try? audioRecorder = AVAudioRecorder(url: filePath, settings: recordSettings)
+        
+        audioRecorder?.delegate = self
+        audioRecorder?.isMeteringEnabled = true
+        audioRecorder?.prepareToRecord()
+        audioRecorder?.record()
     }
 }
 
 // MARK: AVAudioRecorderDelegate
 extension RecordSoundViewController: AVAudioRecorderDelegate {
-    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder?.url)
+        } else {
+            print("recording was not succesful")
+        }
+    }
 }
