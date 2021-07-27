646c51397ebb044164771f536670a87f71b2164c
전소영 <jsu3417@gmail.com>
2021-07-27 15:47:53 +0900

    [#7] audioPlayerDidFinishPlaying 추가

--- VODA_iOS/VODA_iOS/Sources/Audio Record/Audio/AudioPlayManager.swift
+++ VODA_iOS/VODA_iOS/Sources/Audio Record/Audio/AudioPlayManager.swift
@@ -202,6 +202,9 @@ class AudioPlayManager: NSObject {
     }
 }
 
+// MARK: AVAudioPlayerDelegate
 extension AudioPlayManager: AVAudioPlayerDelegate {
-    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        stop()
+    }
 }
