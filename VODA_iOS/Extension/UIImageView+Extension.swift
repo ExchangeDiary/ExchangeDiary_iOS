b6d7f9fa5b079d0740a1754d34e3d27158758c32
조윤영 <yoon980208@gmail.com>
2021-08-03 12:30:07 +0900

    [CREATE] feature/authorization PR + 변수명 수정

---
+++ VODA_iOS/Extension/UIImageView+Extension.swift
@@ -0,0 +1,31 @@
+//
+//  UIImageView+Extension.swift
+//  VODA_iOS
+//
+//  Created by 조윤영 on 2021/08/01.
+//
+
+import UIKit
+import Foundation
+import Kingfisher
+
+extension UIImageView {
+    func makeCircleImageView() {
+        self.layer.cornerRadius = self.layer.frame.height / 2
+        self.layer.masksToBounds = true
+    }
+        
+    public func setImage(_ urlString: String?, defaultImagePath: String) {
+        let defaultImage = UIImage(named: defaultImagePath)
+            
+        if let imageURL = urlString?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+            if imageURL.isEmpty {
+                self.image = defaultImage
+            } else {
+                self.kf.setImage(with: URL(string: imageURL), placeholder: defaultImage, options: [.transition(ImageTransition.fade(0.5))])
+            }
+        } else {
+            self.image = defaultImage
+        }
+    }
+}
