a07f1da1d5d1fcd055940dcce8c9e2a2a30e2cce
Choyoonyoung <yoon980208@gmail.com>
2021-08-07 18:48:17 +0900

    [CREATE] 애플 로그인 연동 구현
    - 애플 로그인 Capability 추가
    - Auth.storyboard, SignInViewController 구현
    - AppleLoginManager 구현

---
+++ VODA_iOS/VODA_iOS/Sources/Auth/ViewController/SignInViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SignInViewController.swift
+//  VODA_iOS
+//
+//  Created by 조윤영 on 2021/08/03.
+//
+
+import UIKit
+import AuthenticationServices
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var kakoLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButton: UIButton!
+    
+    let appleLoginManager = AppleLoginManager()
+    let socialLoginManager = SocialLoginManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func googleLoginTouchUpInsideAction(_ sender: Any) {
+        socialLoginManager.login(with: .google)
+    }
+    
+    @IBAction func kakaoLoginTouchUpInsideAction(_ sender: Any) {
+        socialLoginManager.login(with: .kakao)
+    }
+    
+    @available(iOS 13.0, *)
+    @IBAction func appleLoginTouchUpInsideAction(_ sender: Any) {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let appleRequest = appleIDProvider.createRequest()
+        
+        appleRequest.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [appleRequest])
+        
+        controller.delegate = appleLoginManager
+        controller.presentationContextProvider = appleLoginManager
+        controller.performRequests()
+    }
+}
