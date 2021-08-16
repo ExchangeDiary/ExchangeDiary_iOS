d6adaa9d9ede46b4ec774bb61040fcda1192049a
전소영 <jsu3417@gmail.com>
2021-08-16 12:49:59 +0900

    [#23] 버튼타입, 사진, 작성기한 타입 팝업 생성 및 연결

---
+++ VODA_iOS/VODA_iOS/Sources/PopUp/ViewController/PhotoPopUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoPopUpViewController.swift
+//  VODA_iOS
+//
+//  Created by 전소영 on 2021/08/15.
+//
+
+import UIKit
+
+class PhotoPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
