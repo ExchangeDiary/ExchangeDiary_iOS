25f7704fb1d349b4cd7d349428228116d4a8adee
전소영 <jsu3417@gmail.com>
2021-08-16 14:20:09 +0900

    [#23] 작성기한 팝업 로직 추가

--- VODA_iOS/VODA_iOS/Sources/PopUp/ViewController/WritePeroidPopUpViewController.swift
+++ VODA_iOS/VODA_iOS/Sources/PopUp/ViewController/WritePeroidPopUpViewController.swift
@@ -8,22 +8,42 @@
 import UIKit
 
 class WritePeroidPopUpViewController: UIViewController {
-
+    @IBOutlet weak var writePeriodPickerView: UIPickerView!
+    let periodList = ["1일", "2일", "3일", "4일", "5일", "6일", "7일"]
+    var selectedRow = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        writePeriodPickerView.delegate = self
+        writePeriodPickerView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func selectWritePeriod(_ sender: Any) {
+        //TODO: 선택된 작성 주기 전달하기
+//        periodList[selectedRow]
     }
-    */
+}
 
+// MARK: UIPickerViewDelegate
+extension WritePeroidPopUpViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedRow = row
+        print("selectedRow: \(selectedRow)")
+    }
+}
+
+// MARK: UIPickerViewDataSource
+extension WritePeroidPopUpViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return periodList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return periodList[row]
+    }
 }
