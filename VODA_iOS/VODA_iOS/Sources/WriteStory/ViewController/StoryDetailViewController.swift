0518cdd690f653871001407ced3224a2f16e9b85
전소영 <jsu3417@gmail.com>
2021-08-22 04:29:13 +0900

    [#24] storyDetailViewController 생성

---
+++ VODA_iOS/VODA_iOS/Sources/WriteStory/ViewController/StoryDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StoryDetailViewController.swift
+//  VODA_iOS
+//
+//  Created by 전소영 on 2021/08/22.
+//
+
+import UIKit
+
+class StoryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
