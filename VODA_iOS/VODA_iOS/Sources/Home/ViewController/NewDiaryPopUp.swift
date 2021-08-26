a9ef57b5062fd003e128ebca727dd8019d2561a0
Choyoonyoung <yoon980208@gmail.com>
2021-08-26 18:39:49 +0900

    [UPDATE] 다이어리 생성 화면 구현
    - nextButtonTouchUpInsideAction / moveToNextPage(): 다음 팝업 화면으로 넘어가는 함수
    -> nextButtonTouchUpInsideAction은 추후 삭제 예정
    
    - prevButtonTouchUpInsideAction(): 이전 팝업 화면으로 넘어가는 함수
    
    - completeButtonTouchUpUnsudeAction(): 작성한 내용으로 새로운 다이어리 생성
    
    - closeButtonTouchUpInsideAction(): 팝업 닫힘 함수

---
+++ VODA_iOS/VODA_iOS/Sources/Home/ViewController/NewDiaryPopUp.swift
@@ -0,0 +1,74 @@
+//
+//  NewDiaryPopUp.swift
+//  VODA_iOS
+//
+//  Created by 조윤영 on 2021/08/21.
+//
+
+import UIKit
+import DropDown
+
+class NewDiaryPopUp: UIViewController {
+    var windowHeight = UIScreen.main.bounds.size.height
+    var windowWidth = UIScreen.main.bounds.size.width
+    
+    let createDiaryCollectionViewCellIdentifier = "createDiaryCollectionViewCell"
+    let selectBackgroundIamgeCollectionViewCellIdentifier = "selectBackgroundIamgeCollectionViewCell"
+    let inputJoinCodeCollectionViewCellIdentifier = "inputJoinCodeCollectionViewCell"
+    
+    @IBOutlet weak var newDiaryCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.newDiaryCollectionView.isScrollEnabled = false
+        
+        let gesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapCollectionView(_:)))
+        self.newDiaryCollectionView.addGestureRecognizer(gesture)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+    
+    @objc func tapCollectionView(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    private func visibleCellIndexPath() -> IndexPath {
+        return self.newDiaryCollectionView.indexPathsForVisibleItems[0]
+    }
+    
+    @IBAction func nextButtonTouchUpInsideAction(_ sender: Any) {
+        var item = visibleCellIndexPath().item
+ 
+        item += 1
+        self.newDiaryCollectionView.scrollToItem(at: IndexPath(item: item, section: 0), at: .centeredHorizontally, animated: true)
+    }
+    
+    @IBAction func prevButtonTouchUpInsideAction(_ sender: Any) {
+        var item = visibleCellIndexPath().item
+ 
+        item -= 1
+        self.newDiaryCollectionView.scrollToItem(at: IndexPath(item: item, section: 0),
+                                  at: .centeredHorizontally,
+                                  animated: true)
+    }
+    
+    @IBAction func completeButtonTouchUpInsideAction(_ sender: Any) {
+        // TODO: 다이어리 추가
+        // FIXME: 시연을 위해 present된 팝업화면 사라지게 임시 구현
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeButtonTouchUpInsideAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension NewDiaryPopUp: CreateDiaryCellDelegate {
+    func moveToNextPage() {
+        var item = visibleCellIndexPath().item
+
+        item += 1
+        self.newDiaryCollectionView.scrollToItem(at: IndexPath(item: item, section: 0), at: .centeredHorizontally, animated: true)
+    }
+}
