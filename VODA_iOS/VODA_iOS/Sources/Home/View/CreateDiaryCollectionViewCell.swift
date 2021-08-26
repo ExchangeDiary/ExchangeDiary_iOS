d7fae2bfea545ce46adc1ce7c0d7a80d4ade88b6
Choyoonyoung <yoon980208@gmail.com>
2021-08-26 18:43:06 +0900

    [UPDATE] 다이어리 생성 collectionView 구현
    - 0번째 cell: 다이어리 테마/이름/작성주기 작성 화면
    	- 드롭다운 UI 구현을 위해 dropDown 라이브러리 사용
    - 1번째 cell: 다이어리 배경 설정 화면
    - 2번째 cell: 참여코드 설정 화면
    
    - showWarning(): 다이어리 테마 및 이름 작성 시 공백 허용하지 않고 경고 띄우기

---
+++ VODA_iOS/VODA_iOS/Sources/Home/View/CreateDiaryCollectionViewCell.swift
@@ -0,0 +1,136 @@
+//
+//  CreateDiaryCollectionViewCell.swift
+//  VODA_iOS
+//
+//  Created by 조윤영 on 2021/08/21.
+//
+
+import UIKit
+import DropDown
+
+protocol CreateDiaryCellDelegate: AnyObject {
+    func moveToNextPage()
+}
+
+class CreateDiaryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var diaryThemeView: UIView!
+    @IBOutlet weak var diaryNameView: UIView!
+    @IBOutlet weak var cycleSelectionView: UIView!
+    @IBOutlet weak var cycleSelectionArrowImageView: UIImageView!
+    @IBOutlet weak var cycleSelectionLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var diaryThemeTextfield: UITextField!
+    @IBOutlet weak var diaryNameTextField: UITextField!
+    
+    private let dropDown = DropDown()
+    weak var delegate: CreateDiaryCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        setStyle()
+        setDropDownStyle()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+          self.endEditing(true)
+    }
+    
+    @IBAction func nextButtonTouchUpInsideAction(_ sender: Any) {
+        if self.diaryThemeTextfield.text?.isEmpty == true || self.diaryNameTextField.text?.isEmpty == true {
+            showWarning()
+        } else {
+            self.delegate?.moveToNextPage()
+        }
+    }
+    
+    func showWarning() {
+        if self.diaryThemeTextfield.text?.isEmpty == true { self.diaryThemeView.layer.borderColor = UIColor.red.cgColor
+        }
+        
+        if self.diaryNameTextField.text?.isEmpty == true {
+            self.diaryNameView.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+    
+    @IBAction func cycleSelectionButtonTouchUpInsideAction(_ sender: Any) {
+        dropDown.show()
+        changeCycleSelectionViewStyle()
+    }
+    
+    func changeCycleSelectionViewStyle() {
+        self.cycleSelectionView.layer.borderColor = UIColor.CustomColor.vodaMainBlue.cgColor
+        self.cycleSelectionArrowImageView.image = UIImage(named: "arrow_down_activate")
+    }
+    
+    func initCycleSelectionViewStyle() {
+        self.cycleSelectionView.layer.borderColor = UIColor.CustomColor.gray5.cgColor
+        self.cycleSelectionArrowImageView.image = UIImage(named: "arrow_down_inactivate")
+    }
+    
+    func setStyle() {
+        self.contentView.layer.cornerRadius = 16
+        self.contentView.layer.masksToBounds = true
+        self.layer.cornerRadius = 16
+        self.layer.masksToBounds = false
+        self.addShadow(width: 3, height: 3, radius: 16, opacity: 0.16)
+        
+        self.diaryThemeView.layer.cornerRadius = 8
+        self.diaryThemeView.layer.borderWidth = 1
+        self.diaryThemeView.layer.borderColor = UIColor.CustomColor.gray5.cgColor
+        
+        self.diaryNameView.layer.cornerRadius = 8
+        self.diaryNameView.layer.borderWidth = 1
+        self.diaryNameView.layer.borderColor = UIColor.CustomColor.gray5.cgColor
+        
+        self.cycleSelectionView.layer.cornerRadius = 8
+        self.cycleSelectionView.layer.borderWidth = 1
+        self.initCycleSelectionViewStyle()
+        
+        self.nextButton.layer.cornerRadius = 8
+    }
+    
+    func setDropDownStyle() {
+        dropDown.cellHeight = self.contentView.bounds.height / 16
+        dropDown.direction = .bottom
+        dropDown.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        dropDown.textColor = #colorLiteral(red: 0.737254902, green: 0.737254902, blue: 0.737254902, alpha: 1)
+        dropDown.textFont = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)!
+        dropDown.layer.borderColor = UIColor.CustomColor.gray6.cgColor
+        dropDown.layer.borderWidth = 1
+        dropDown.makeBottomSectionRound(8)
+        dropDown.shadowRadius = 0
+        dropDown.animationEntranceOptions = .transitionCurlDown
+    
+        dropDown.dataSource = ["1일", "2일", "3일", "4일", "5일", "6일", "7일"]
+        dropDown.anchorView = self.cycleSelectionView
+        dropDown.width = self.cycleSelectionView.bounds.width
+        if let dropDownHeight = dropDown.anchorView?.plainView.bounds.height {
+            dropDown.bottomOffset = CGPoint(x: 0, y: dropDownHeight)
+        }
+        
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            print(index)
+            self.cycleSelectionLabel.text = item
+            self.cycleSelectionLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            self.initCycleSelectionViewStyle()
+        }
+    }
+}
+
+extension CreateDiaryCollectionViewCell: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField {
+        case self.diaryThemeTextfield:
+            self.diaryThemeView.layer.borderColor = UIColor.CustomColor.gray5.cgColor
+        case self.diaryNameTextField:
+            self.diaryNameView.layer.borderColor = UIColor.CustomColor.gray5.cgColor
+        default:
+            break
+        }
+    }
+}
---
+++ VODA_iOS/VODA_iOS/Sources/Home/ViewController/NewDiaryPopUp+CollectionViewExtension.swift
@@ -0,0 +1,49 @@
+//
+//  NewDiaryPopUp+CollectionViewExtension.swift
+//  VODA_iOS
+//
+//  Created by 조윤영 on 2021/08/26.
+//
+
+import UIKit
+
+extension NewDiaryPopUp: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row == 0 {
+            if let createDiaryCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: createDiaryCollectionViewCellIdentifier, for: indexPath) as? CreateDiaryCollectionViewCell {
+                createDiaryCollectionViewCell.delegate = self
+                return createDiaryCollectionViewCell
+            }
+        } else if indexPath.row == 1 {
+            if let selectBackgroundIamgeCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: selectBackgroundIamgeCollectionViewCellIdentifier, for: indexPath) as? SelectBackgroundImageCollectionViewCell {
+                    return selectBackgroundIamgeCollectionViewCell
+            }
+        } else {
+            if let inputJoinCodeCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: inputJoinCodeCollectionViewCellIdentifier, for: indexPath) as? InputJoinCodeCollectionViewCell {
+                    return inputJoinCodeCollectionViewCell
+            }
+        }
+        return UICollectionViewCell()
+    }
+}
+
+extension NewDiaryPopUp: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: windowHeight / 5.13, left: windowWidth / 11.3, bottom: windowHeight / 5.13, right: windowWidth / 11.3)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return windowWidth / 11.3 * 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = Int(windowWidth / 1.2)
+        let height = Int(windowHeight / 1.63)
+    
+        return CGSize(width: width, height: height)
+    }
+}
