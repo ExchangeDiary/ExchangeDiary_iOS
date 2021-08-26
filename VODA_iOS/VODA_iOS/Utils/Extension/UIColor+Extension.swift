719a0a8e1e57f2c4de569e512edb5d2360ac87b5
Choyoonyoung <yoon980208@gmail.com>
2021-08-27 00:15:29 +0900

    [UPDATE] 컬러셋 정리

--- VODA_iOS/VODA_iOS/Utils/Extension/UIColor+Extension.swift
+++ VODA_iOS/VODA_iOS/Utils/Extension/UIColor+Extension.swift
@@ -13,10 +13,10 @@ extension UIColor {
         static let vodaMainBlue = #colorLiteral(red: 0.2509803922, green: 0.5058823529, blue: 1, alpha: 1)
         static let vodaSubBlue = #colorLiteral(red: 0.6862745098, green: 0.7921568627, blue: 1, alpha: 1)
         
-        static let gray2 = #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)
-        static let gray5 = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1)
-        static let gray6 = #colorLiteral(red: 0.6196078431, green: 0.6196078431, blue: 0.6196078431, alpha: 1)
+        static let vodaGray2 = #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)
         static let vodaGray3 = #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1)
         static let vodaGray4 = #colorLiteral(red: 0.8784313725, green: 0.8784313725, blue: 0.8784313725, alpha: 1)
+        static let vodaGray5 = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1)
+        static let vodaGray6 = #colorLiteral(red: 0.6196078431, green: 0.6196078431, blue: 0.6196078431, alpha: 1)
     }
 }
