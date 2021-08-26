8217469fd7a9b4c8fdf34aa44ef242987316462b
Choyoonyoung <yoon980208@gmail.com>
2021-08-26 18:43:23 +0900

    [UPDATE] 기본 커스텀 색 추가 업로드

--- VODA_iOS/VODA_iOS/Utils/Extension/UIColor+Extension.swift
+++ VODA_iOS/VODA_iOS/Utils/Extension/UIColor+Extension.swift
@@ -12,5 +12,9 @@ extension UIColor {
         static let vodaYellow = #colorLiteral(red: 1, green: 0.7725490196, blue: 0.1921568627, alpha: 1)
         static let vodaMainBlue = #colorLiteral(red: 0.2509803922, green: 0.5058823529, blue: 1, alpha: 1)
         static let vodaSubBlue = #colorLiteral(red: 0.6862745098, green: 0.7921568627, blue: 1, alpha: 1)
+        
+        static let gray2 = #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)
+        static let gray5 = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1)
+        static let gray6 = #colorLiteral(red: 0.6196078431, green: 0.6196078431, blue: 0.6196078431, alpha: 1)
     }
 }
