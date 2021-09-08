31f330880b57d2842b80b8b8ff0a0e777bc74f0e
전소영 <jsu3417@gmail.com>
2021-09-08 16:09:34 +0900

    [#31] push 알림 화면 구현

--- VODA_iOS/VODA_iOS/Sources/Push/ViewController/PushViewController.swift
+++ VODA_iOS/VODA_iOS/Sources/Push/ViewController/PushViewController.swift
@@ -8,21 +8,12 @@
 import UIKit
 
 class PushViewController: UIViewController {
-
+    @IBOutlet weak var pushView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.setNavigationBarColor(color: .clear)
+        pushView.addShadow(width: 0, height: -4, radius: 8, opacity: 0.1)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
