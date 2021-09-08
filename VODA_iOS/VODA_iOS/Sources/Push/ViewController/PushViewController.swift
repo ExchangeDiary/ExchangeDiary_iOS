2348f6baf6b99a67beaa7fcb0a437f6e51c20b81
전소영 <jsu3417@gmail.com>
2021-09-08 17:21:30 +0900

    [#31] pushCollectionVIewCell 추가

--- VODA_iOS/VODA_iOS/Sources/Push/ViewController/PushViewController.swift
+++ VODA_iOS/VODA_iOS/Sources/Push/ViewController/PushViewController.swift
@@ -17,3 +17,26 @@ class PushViewController: UIViewController {
         pushView.addShadow(width: 0, height: -4, radius: 8, opacity: 0.1)
     }
 }
+
+// MARK: UICollectionViewDataSource
+extension PushViewController: UICollectionViewDataSource {
+    //FIXME: 현재 테스트만 완료, 서버 연동 필요
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PushCollectionViewCell", for: indexPath)
+        let pushCollectionViewCell = cell as? PushCollectionViewCell
+        
+        return cell
+    }
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension PushViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: Int(DeviceInfo.screenWidth), height: 87)
+    }
+}
