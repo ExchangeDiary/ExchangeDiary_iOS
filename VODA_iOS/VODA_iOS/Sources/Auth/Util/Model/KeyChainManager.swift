2eeb877bb046497b6785bce695953c293bcdc5f2
Choyoonyoung98 <yoon980208@gmail.com>
2022-03-11 20:54:30 +0900

    [CREATE] 로컬 토큰 관리 로직 구현
    - 토큰을 안전하게 저장 및 삭제할 KeyChainManager
    - 토큰 갱신 및 발급을 위한 TokenManager

---
+++ VODA_iOS/VODA_iOS/Sources/Auth/Util/Model/KeyChainManager.swift
@@ -0,0 +1,59 @@
+//
+//  KeychainManager.swift
+//  VODA_iOS
+//
+//  Created by 조윤영 on 2022/03/10.
+//
+
+import Foundation
+import Security
+
+//TODO: 이거 로그인하면서 토큰값 저장하는 부분 구현하기
+class KeyChainManager {
+    func setTokenValue(_ service: String, account: String, value: String) {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecValueData: value.data(using: .utf8, allowLossyConversion: false)!
+        ]
+         
+        SecItemDelete(keyChainQuery)
+        
+        let status: OSStatus = SecItemAdd(keyChainQuery, nil)
+        assert(status == noErr, "토큰 값 저장에 실패했습니다")
+    }
+    
+    func getTokenValue(_ service: String, account: String) -> String? {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecReturnData: kCFBooleanTrue ?? true,
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
+
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(keyChainQuery, &dataTypeRef)
+            
+        if (status == errSecSuccess) {
+            let retrievedData = dataTypeRef as! Data
+            let value = String(data: retrievedData, encoding: String.Encoding.utf8)
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    func deleteTokenValue(_ service: String, account: String) {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account
+        ]
+            
+        let status = SecItemDelete(keyChainQuery)
+        print(status)
+    }
+}
+
