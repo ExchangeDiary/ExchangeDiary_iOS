80dd6a6236b688edcada2e99500136cc8ff571aa
전소영 <jsu3417@gmail.com>
2022-03-15 15:44:17 +0900

    [#68] push 로직 삭제

--- VODA_iOS/VODA_iOS/Sources/Main/ViewController/MainViewController.swift
+++ VODA_iOS/VODA_iOS/Sources/Main/ViewController/MainViewController.swift
@@ -8,30 +8,28 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    @IBOutlet weak var pushView: UIView!
     @IBOutlet weak var homeView: UIView!
     @IBOutlet weak var myPageView: UIView!
     @IBOutlet weak var tabBar: UITabBar!
     @IBOutlet weak var homeTabBarItem: UITabBarItem!
+    @IBOutlet weak var myPageTabBarItem: UITabBarItem!
     @IBOutlet weak var shadowView: UIView!
-    private var pushViewController: PushViewController?
     private var homeViewController: HomeViewController?
     private var myPageViewController: MyPageViewController?
+    private var selectedTabBarItemIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTabBarUI()
-
+       
         setRootViewController(rootViewController: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
-        case MainTab.push.segueIdentifier:
-            let navigationController = segue.destination as? UINavigationController
-            pushViewController = navigationController?.topViewController as? PushViewController
         case MainTab.home.segueIdentifier:
-            homeViewController = segue.destination as? HomeViewController
+            let navigationController = segue.destination as? UINavigationController
+            homeViewController = navigationController?.topViewController as? HomeViewController
         case MainTab.myPage.segueIdentifier:
             let navigationController = segue.destination as? UINavigationController
             myPageViewController = navigationController?.topViewController as? MyPageViewController
