484987f3fc3827a37b92e33a4308b1278da4ad44
Choyoonyoung98 <yoon980208@gmail.com>
2022-03-23 21:28:51 +0900

    [CREATE] 새로 추가된 개인정보처리방침 관련 ViewController 구현
    - TOSNavigationController
    - TOSViewController
    - TOSWebViewController
    
    *TOS = Terms of Service

---
+++ VODA_iOS/VODA_iOS/Sources/Auth/ViewController/TOSViewController.swift
@@ -0,0 +1,96 @@
+//
+//  TOSViewController.swift
+//  VODA_iOS
+//
+//  Created by 조윤영 on 2022/03/20.
+//
+
+import UIKit
+import WebKit
+
+class TOSViewController: UIViewController {
+    @IBOutlet weak var fullAgreementCheckBoxButton: UIButton!
+    @IBOutlet weak var over14yearsOldCheckBoxButton: UIButton!
+    @IBOutlet weak var TOSAgreementCheckBoxButton: UIButton!
+    @IBOutlet weak var privacyPolicyAgreementCheckBoxButton: UIButton!
+    @IBOutlet weak var completeButton: UIButton!
+    
+    @IBOutlet weak var showTOSAgreementDetailButton: UIButton!
+    @IBOutlet weak var showPrivacyAgreementDetailButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setCheckBoxStatus()
+        setCompleteButtonDisabled()
+    }
+    
+    func setStyle() {
+        self.completeButton.makeCornerRadius(radius: 16)
+    }
+    
+    @IBAction func checkBoxTouchUpInsideAction(_ sender: UIButton) {
+        if sender == self.fullAgreementCheckBoxButton {
+            let agreement = !self.fullAgreementCheckBoxButton.isSelected
+            self.fullAgreementCheckBoxButton.isSelected = agreement
+            self.over14yearsOldCheckBoxButton.isSelected = agreement
+            self.TOSAgreementCheckBoxButton.isSelected = agreement
+            self.privacyPolicyAgreementCheckBoxButton.isSelected = agreement
+        } else { sender.isSelected.toggle() }
+        
+        if checkIsCheckBoxAllSelected() {
+            setCompleteButtonEnable()
+            self.fullAgreementCheckBoxButton.isSelected = true
+        } else {
+            setCompleteButtonDisabled()
+            self.fullAgreementCheckBoxButton.isSelected = false
+        }
+    }
+    
+    func checkIsCheckBoxAllSelected() -> Bool {
+        if self.over14yearsOldCheckBoxButton.isSelected && self.TOSAgreementCheckBoxButton.isSelected && self.privacyPolicyAgreementCheckBoxButton.isSelected {
+            return true
+        }
+        
+        return false
+    }
+    
+    @IBAction func showAgreementDetail(_ sender: UIButton) {
+        if let TOSWebViewController = UIStoryboard(name: Storyboard.auth.name, bundle: nil).instantiateViewController(withIdentifier: "TOSWebViewController") as? TOSWebViewController {
+            var agreementUrl: URL?
+            if sender == self.showTOSAgreementDetailButton {
+                agreementUrl = TOSUrl.TOSAgreementUrl
+            } else {
+                agreementUrl = TOSUrl.PrivacyPolicyAgreemntUrl
+            }
+            
+            TOSWebViewController.modalPresentationStyle = .fullScreen
+            TOSWebViewController.url = agreementUrl
+            self.navigationController?.pushViewController(TOSWebViewController, animated: true)
+        }
+    }
+    
+    func setCheckBoxStatus() {
+        let checkBoxNoneImage = UIImage(named: "checkBox_none")
+        let checkBoxActiveImage = UIImage(named: "checkBox_active")
+        
+        self.fullAgreementCheckBoxButton.setImage(checkBoxNoneImage, for: .normal)
+        self.fullAgreementCheckBoxButton.setImage(checkBoxActiveImage, for: .selected)
+        self.over14yearsOldCheckBoxButton.setImage(checkBoxNoneImage, for: .normal)
+        self.over14yearsOldCheckBoxButton.setImage(checkBoxActiveImage, for: .selected)
+        self.TOSAgreementCheckBoxButton.setImage(checkBoxNoneImage, for: .normal)
+        self.TOSAgreementCheckBoxButton.setImage(checkBoxActiveImage, for: .selected)
+        self.privacyPolicyAgreementCheckBoxButton.setImage(checkBoxNoneImage, for: .normal)
+        self.privacyPolicyAgreementCheckBoxButton.setImage(checkBoxActiveImage, for: .selected)
+    }
+    
+    func setCompleteButtonDisabled() {
+        self.completeButton.isEnabled = false
+        self.completeButton.backgroundColor = UIColor.CustomColor.vodaGray4
+    }
+    
+    func setCompleteButtonEnable() {
+        self.completeButton.isEnabled = true
+        self.completeButton.backgroundColor = UIColor.CustomColor.vodaMainBlue
+    }
+}
